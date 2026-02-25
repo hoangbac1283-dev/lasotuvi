ff5935db535e5d746fda3b6058ad08ff7504c009
Bacht1 <hoangbac@Bacht1s-MacBook-Pro.local>
2026-02-25 09:58:59 +0700

    update API to new google genai sdk

--- API.py
+++ API.py
@@ -79,7 +79,7 @@ async def chat(request: Request, body: dict):
     """
 
     response = client.models.generate_content(
-        model="gemini-1.5-flash",
+    model="gemini-2.0-flash",
         contents=prompt,
     )
 
